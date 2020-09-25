97a3ff84d71a3dab1dcd8789f25e7c69802cf21f
ayush <56297753+ayushsharma11@users.noreply.github.com>
2020-09-25 20:02:53 +0530

    Add files via upload

---
+++ spring-aop-advice-examples/src/main/java/net/guides/springboot2/springboot2jpacrudexample/aspect/LoggingAspect.java
@@ -0,0 +1,55 @@
+package net.guides.springboot2.springboot2jpacrudexample.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+@Aspect
+@Component
+public class LoggingAspect {
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	@Before("execution(* net.guides.springboot2.springboot2jpacrudexample.service.FollowerService.addFollower(..))")
+	public void logBefore(JoinPoint joinPoint) {
+		log.debug("logBefore running .....");
+
+	}
+	@After("execution(* net.guides.springboot2.springboot2jpacrudexample.service.FollowerService.addFollower(..))")
+	public void logAfter(JoinPoint joinPoint) {
+		log.debug("logAfter running .....");
+
+	}
+	@AfterReturning(pointcut = "execution(* net.guides.springboot2.springboot2jpacrudexample.service.FollowerService.deleteFollower(..))", returning = "result")
+	public void logAfterReturning(JoinPoint joinPoint, Object result) {
+		log.debug("logAfterReturning running .....");
+
+
+	}
+
+
+	@Around("execution(* net.guides.springboot2.springboot2jpacrudexample.service.FollowerService.getFollowerById(..))")
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		log.debug("logAround running .....");
+
+		try {
+			Object result = joinPoint.proceed();
+			return result;
+		} catch (IllegalArgumentException e) {
+
+			throw e;
+		}
+
+	}
+
+	@AfterThrowing(pointcut = "execution(* net.guides.springboot2.springboot2jpacrudexample.service.FollowerService.updateFollower(..))", throwing = "error")
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
+		log.debug("logAfterThrowing running .....");
+
+	}
+}
